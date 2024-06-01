0c6adb5a02735d7ddf1db54c1fc204df3a96a6ef
Elisée Houessenon <ebhouessenon@gmail.com>
2024-06-01 10:35:34 +0100

    Commitment of my portfolio

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+
+//typing animation script
+
+var typed = new Typed(".typing", {
+     strings: [ "", "Software Developer"," Programmer ", " Web Developer ", " Web Designer"," Translator"],
+     typeSpeed:100,
+     backSpeed:60,
+     loop:true
+})
+var typed = new Typed(".typing-2", {
+     strings: [ "", " Programmer ", " Web Developer ", " Web Designer"," Translator" ,  "Freelancer"],
+     typeSpeed:100,
+     backSpeed:60,
+     loop:true
+})
+
